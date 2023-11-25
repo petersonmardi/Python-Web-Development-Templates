2d50392a445e81d46971fd21b733f2febcce88fb
petersonmardi <petersonmardi@gmail.com>
2023-11-24 22:56:14 -0400

    Rewrite the SECRET KEY var

--- my-app/__init__.py
+++ my-app/__init__.py
@@ -9,26 +9,22 @@ import os
 def create_app(config=None):
     app = Flask(__name__, instance_relative_config=False)
 
-    key_value = []
-    random_key = os.urandom(16)
-    key_value.append(random_key)
-
     app.config.from_mapping(
-        SECRET_KEY=key_value[0],
+        SECRET_KEY=os.uraqndom(32),
         # SQLALCHEMY_DATABASE_URI = "sqlite:///instance/database/database.db",
         # SQLALCHEMY_TRACK_MODIFICATIONS = False,
     )
 
-   # db.init_app(app)
-   # migrate.init_app(app, db)
+    # db.init_app(app)
+    # migrate.init_app(app, db)
 
-   # with app.app_context():
+    # with app.app_context():
     #    db.create_all()
 
     @app.route('/hello')
     def hello():
         return "Hello, World!"
-        
+
     from . import index
     app.register_blueprint(index.bp)
 

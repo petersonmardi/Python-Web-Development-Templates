ec8367c2f3ff0cb027b1a44e05e98f578aa48522
Peterson Mardi <58122467+petersonmardi@users.noreply.github.com>
2024-01-08 14:48:58 -0400

    Update manage.py
    Run deployment tasks

--- my-app/manage.py
+++ my-app/manage.py
@@ -1,1 +1,20 @@
-RUN DEPLOYMENT TASKS 
+def deploy():
+    """RUN DEPLOYMENT TASKS"""
+    from my-app import create_app, db
+    from flask_migrate import upgrade, migrate, init, stamp
+    from models import User
+
+
+    app = create_app()
+    app.app_context().push()
+
+    # create database and tables
+    db.create_all()
+
+    # migrate database to latest revision
+    stamp()
+    migrate()
+    upgrade()
+
+
+deploy()
